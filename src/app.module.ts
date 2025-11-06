6591e77f913cff78a3f4267ad295bb03d9fee02c
Flavio Serra <sonyflavio@gmail.com>
2025-11-06 10:37:25 -0300

    Implementação da ProdService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApoliceModule } from './apolice/apolice.module';
-import { Apolice } from './apolice/entities/apolice.entity';
 import { CategoriaModule } from './categoria/categoria.module';
-import { Categoria } from './categoria/entities/categoria.entity';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_seguro_vida',
-      entities: [Usuario, Apolice, Categoria],
-      synchronize: true,
-      logging: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     UsuarioModule,
     ApoliceModule,
